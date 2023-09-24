a640177fcdbbdc8e2bbdcf52f7fb15f8c815ca51
bei-m <116886672+bei-m@users.noreply.github.com>
2023-09-24 14:05:09 +0300

    add files

---
+++ hash/mylib.h
@@ -0,0 +1,34 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+#include <iostream> 
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <random>
+#include <fstream>
+#include <chrono>
+#include <sstream>
+#include <codecvt>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::fixed;
+using std::ifstream;
+using std::uniform_int_distribution;
+using namespace std::chrono;
+using std::ofstream;
+using std::istream;
+using std::wstringstream;
+using std::wstring;
+using std::wcin;
+using std::wcout;
+using std::locale;
+using std::wifstream;
+using std::swap;
+using std::locale;
+using std::codecvt_utf8;
+
+#endif
